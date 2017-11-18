e059c9a880d8812fe2925e64dc78f0c9505fd628
akosala <akosala@wp.pl>
2017-11-18 18:06:12 +0100

    Działa poprawka

--- src/main/java/searchengine/servlets/HelloServlet.java
+++ src/main/java/searchengine/servlets/HelloServlet.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
+@WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        writer.println("Hello World!");
+        writer.println("Adam");
     }
 }
